7a9e23bd8663136f829c97c9354cca3c3e3c5aea
Brock Hemsouvanh <brox0rz@gmail.com>
2024-03-24 18:23:59 -0700

    Update app.module.ts with attribution and clarifying declarations

--- profile-app/src/app/app.module.ts
+++ profile-app/src/app/app.module.ts
@@ -1,6 +1,12 @@
+/**
+ * File name: app.module.ts
+ * File description: Root module of the Angular application where app components are declared.
+ * Author(s): Brock Hemsouvanh and Professor Richard Krasso
+ * Date created: 2024-03-24
+ */
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyImageComponent } from './my-image/my-image.component';
