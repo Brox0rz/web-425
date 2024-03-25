01ca9dd80eadd49b280ab1fd109d89b0573f5f5b
Brock Hemsouvanh <brox0rz@gmail.com>
2024-03-24 18:32:13 -0700

    Update my-details.component.ts
    add attribution

--- profile-app/src/app/my-details/my-details.component.ts
+++ profile-app/src/app/my-details/my-details.component.ts
@@ -1,3 +1,10 @@
+/**
+ * File name: my-details.component.ts
+ * File description: Component logic for my-details.
+ * Author(s): Brock Hemsouvanh and Professor Richard Krasso
+ * Date created: 2024-03-24
+ */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
