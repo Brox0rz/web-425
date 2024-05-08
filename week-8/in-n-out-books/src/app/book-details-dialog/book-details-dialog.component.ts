5afe73cde03648964f63581f402457f655d04b08
Brox0rz <brox0rz@gmail.com>
2024-05-08 09:36:49 -0700

    updated comments on book-details-dialog.component.ts

--- week-8/in-n-out-books/src/app/book-details-dialog/book-details-dialog.component.ts
+++ week-8/in-n-out-books/src/app/book-details-dialog/book-details-dialog.component.ts
@@ -2,8 +2,10 @@
  * Title: book-details-dialog.component.ts
  * Authors: Professor Richard Krasso and Brock Hemsouvanh
  * Date: 4/21/2024
+ * Modified: 05/08/2024
  * Description: Book Details component to display and interact with the book details.
  */
+
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { IBook } from '../book.interface';
@@ -15,12 +17,13 @@ import { IBook } from '../book.interface';
 })
 export class BookDetailsDialogComponent {
 
-  book: IBook;
+  book: IBook; // Variable to hold the book data passed from the parent component
 
   constructor(
     public dialogRef: MatDialogRef<BookDetailsDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
+    @Inject(MAT_DIALOG_DATA) public data: any // Injects data passed via MatDialog's `data` property
   ) {
+    // Initialize the `book` variable with the data passed from the parent component
     this.book = data.book;
   }
 }
